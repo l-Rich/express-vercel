f38992e84fa67a60c3a6430a92474523712e0cb5
lauren-richardson <lauren.richardson13@gmail.com>
2024-05-23 13:33:33 -0400

    node version

--- api/product.js
+++ api/product.js
@@ -8,10 +8,7 @@ const router = express.Router();
  */
 router.get("/", async (req, res) => {
   try {
-    res.json({
-      status: 200,
-      message: "Get data has successfully",
-    });
+    res.render('index.html');
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
