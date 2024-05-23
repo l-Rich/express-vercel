ba7dddbfa33c398215c43600b9bdf3c3d644321d
lauren-richardson <lauren.richardson13@gmail.com>
2024-05-23 14:08:52 -0400

    node error

--- api/product.js
+++ api/product.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'));
 /**
  * GET product list.
  *
@@ -10,7 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
  */
 router.get("/", async (req, res) => {
   try {
-    res.sendFile('index.ejs');
+    res.render('index.ejs');
   } catch (error) {
     console.error(error);
     return res.status(500).send("Server error");
